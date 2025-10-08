3be44e7a0be0ab8b9f9920a9a57a51f751f68c10
noelav07 <noel.a.v.07@gmail.com>
2025-10-08 04:03:03 +0000

    fixed ami

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,3 +1,21 @@
+data "aws_ami" "latest_amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"] # This is the AWS account ID for official images
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+
+
+
 resource "aws_key_pair" "this" {
   key_name   = var.key_name
   public_key = var.public_key
@@ -5,7 +23,7 @@ resource "aws_key_pair" "this" {
 
 resource "aws_instance" "instances" {
   count                       = var.instance_count
-  ami                         = var.ami_id
+  ami                         = data.aws_ami.latest_amazon_linux.id
   instance_type               = var.instance_type
   subnet_id                   = var.subnet_ids[count.index]
   vpc_security_group_ids      = var.security_group_ids
